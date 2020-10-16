42584a9858291eee7c6c52a4cfb0d69793966fea
jddarby <67566019+jddarby1108@users.noreply.github.com>
2020-10-15 23:24:56 -0500

    adding queries through tobs

--- Resources/app.py
+++ Resources/app.py
@@ -7,7 +7,7 @@ import os
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect, func
 
 from flask import Flask, jsonify
 
@@ -60,9 +60,53 @@ def names():
 
     # Convert list of tuples into normal list
     all_names = list(np.ravel(results))
+    
     print(all_names)
     return jsonify(all_names)
 
+    # Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
+    # all_prcp = []
+    # for row in results:
+    #     prcp_dict = {}
+    #     row.date = row.prcp
+    #     prcp_dict = {"date", "prcp"}
+    #     all_prcp.append(prcp_dict)
+    # # print(all_prcp)
+    # return jsonify(all_prcp)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a list of all precipitation data"""
+
+    # Query all passengers
+    session = Session(engine)
+    results = session.query(Station.name).all()
+    
+    # close the session to end the communication with the database
+    session.close()
+
+    all_stations = list(np.ravel(results))
+    
+    print(all_stations)
+    return jsonify(all_stations)
+
+    
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Return a list of all tobs data"""
+
+    # Query all most active station
+    session = Session(engine)
+    results = session.query(Measurement.station, func.count(Measurement.tobs)).group_by(Measurement.station).order_by(func.count(Measurement.tobs)).all()[-1]
+    
+    # close the session to end the communication with the database
+    session.close()
+
+    all_tobs = list(np.ravel(results))
+    
+    print(all_tobs)
+    return jsonify(all_tobs)
 
 # @app.route("/api/v1.0/passengers")
 # def passengers():
